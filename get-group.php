1ab5aff2a81107b87b3898c37796d5e374a5a5ba
Austin Tindle <tindleaj@gmail.com>
2017-06-15 21:02:23 -0400

    added alumni property to group members

--- get-group.php
+++ get-group.php
@@ -5,7 +5,7 @@
 require_once('../include/phpScripts/database.inc.php');
 
 /* construct the SQL statement */
-$sql = "SELECT `group_members`.`id`, `firstName`, `lastName`, `affiliations`, `education`, `email`, `research`, `image`, `title`, `groups` FROM `group_members`";
+$sql = "SELECT `group_members`.`id`, `firstName`, `lastName`, `affiliations`, `education`, `email`, `research`, `image`, `title`, `groups`, 'alumni' FROM `group_members`";
 
 /* execute SQL statement and store the result */
 $result = mysql_query($sql);
@@ -28,7 +28,8 @@ while ($row = mysql_fetch_assoc($result)) {
 		'research' => $row['research'],
 		'image' => $row['image'],
 		'title' => $row['title'],
-		'groups' => $row['groups']
+		'groups' => $row['groups'],
+		'alumni' => $row['alumni']
 	);
 	array_push($output, $post);
 }
